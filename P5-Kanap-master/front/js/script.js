f6fe9f8c8bb748a0aa801f2083cb2a982ec63821
Sacha AIT SALAH <Sacha.ait@gmail.com>
2022-06-21 14:27:09 +0200

    Création de la fonction d'appel du tableau, création de la boucle de création des éléments d'affichange des cartes sur index

--- P5-Kanap-master/front/js/script.js
+++ P5-Kanap-master/front/js/script.js
@@ -1,2 +1,64 @@
+main();
 
- 
+function main() {
+  getArticles();
+}
+
+// Récupérer les articles depuis l'API
+function getArticles() {
+  fetch("http://localhost:3000/api/products")
+    .then(function (res) {
+      return res.json();
+    })
+    
+    // Dispatcher les données de chaque produit (prix, nom...) dans le DOM
+    .then(function (resultatAPI) {
+      const articles = resultatAPI;
+      console.log(articles);
+
+      for (let article in articles) {
+        //Création d'une variable contenant la section de classe items
+        let sectionCarte = document.getElementById("items");
+
+        //Création du lien a dans section items
+        let carteProduit = document.createElement("a");
+        //definition de a commen enfant de items 
+        sectionCarte.appendChild(carteProduit);
+        //ajout du href en lien avec l'api
+        carteProduit.href = "product.html?id=${resultatAPI[article]._id}";
+
+        //création balise article dans a
+        let articleProduit = document.createElement("article");
+        //Definition de article comme enfant de a
+        carteProduit.appendChild(articleProduit);
+
+        //création img dans article
+        let imgProduit = document.createElement("img");
+        //definition de img comme enfant de article
+        articleProduit.appendChild(imgProduit);
+        //ajout du src en lien avec .imageUrl 
+        imgProduit.src = resultatAPI[article].imageUrl;
+        //ajout du alt de l'image en lien avec .altTxt
+        imgProduit.alt = resultatAPI[article].altTxt;
+
+        //création h3 dans article
+        let nomProduit = document.createElement("h3");
+        //défition de h3 comme enfant de article
+        articleProduit.appendChild(nomProduit);
+        //ajout de la classe
+        nomProduit.classList.add("productName");
+        //ajout de .name dans innerhtml
+        nomProduit.innerHTML = resultatAPI[article].name;
+
+        //création du p dans article
+        let descriptionProduit = document.createElement("p");
+        //Définition de P comme enfant de article
+        articleProduit.appendChild(descriptionProduit);
+        //ajout de la classe
+        descriptionProduit.classList.add("productDescription");
+        //ajout de .description dans le innerHtml
+        descriptionProduit.innerHTML = resultatAPI[article].description;
+
+      }
+    });
+}
