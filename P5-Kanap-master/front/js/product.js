0354a4359e3a9e8c2c85f92e9ea225bf4163416f
Sacha AIT SALAH <Sacha.ait@gmail.com>
2022-07-12 14:57:01 +0200

    Modif fonction addtocart et ajout de la value de colors

--- P5-Kanap-master/front/js/product.js
+++ P5-Kanap-master/front/js/product.js
@@ -12,6 +12,7 @@ let selectionCouleur = document.getElementById("colors");
 
 let quantitySelection = document.getElementById("quantity");
 let ZoneBouton = document.getElementById("item__content__addButton");
+const AjoutPanier = document.getElementById("addToCart");
 
 main();
 
@@ -35,9 +36,9 @@ function getArticles(){
 
             //création balise img et définition du parents, du alt et du src
             let imgProduit = document.createElement("img");
-            imgItem.appendChild(imgProduit);
             imgProduit.src = articles.imageUrl;
             imgProduit.alt = articles.altTxt;
+            imgItem.appendChild(imgProduit);
             //Modification balise h1 avec le nom produit
             title.textContent = articles.name;
             //Modification du span price avec prix produit
@@ -45,64 +46,71 @@ function getArticles(){
             //Modification du p description avec description produit 
             description.textContent = articles.description;
             //Variable contenant largeur array colors
+
+
             let tableauCouleur = articles.colors.length;
             //Boucle de création des option de couleurs
             for (let i = 0; i< tableauCouleur; i++){
                 let option = document.createElement("option");
                 option.textContent = articles.colors[i];
+                option.setAttribute("value", articles.colors[i]);
                 selectionCouleur.appendChild(option);
             }
-            
-
-
-
         }
 })};
 
 function addToCart() {
-    const AjoutPanier = document.getElementById("addToCart");
+  //Tableau ItemPanier contenant Item
+let ItemPanier = {'Item':[]}
 
-    let imgProduit = document.querySelector("img");
+//On écoute l'évènement click sur AjoutPanier, au click, on éxecute les commandes si dessous 
+AjoutPanier.addEventListener("click", function(event){
+  event.preventDefault();
 
-    // let getelementby tag name(img)
-    // let imgproduit = image[index img 5 ]
+//Récup couleur et quantité sélectionné
+const Couleurselec = document.getElementById("colors").value
+const QuantitySelec = document.getElementById("quantity").value
 
-    let option = document.querySelector("option");
+// Vérif Quantité sélectionné =0; affichange message érreur 
+if (QuantitySelec === "0") {
+  console.log("La quantité doit être supérieur à 0");
+  // Vérif si quantité et couleur sélec. sont déja dans le tableau
+} else if ((ItemPanier.Item.find(item => item.id === id && item.color === Couleurselec))) {
+  console.log(ItemPanier);
+  //Trouve l'index de id et de color
+  let itemIndex = ItemPanier.Item.findIndex(item => item.id === id && item.color === Couleurselec)
 
-    AjoutPanier.addEventListener("click", function(event)  {
-        event.preventDefault();
-      if (quantitySelection.value > 0 && quantitySelection.value < 101) {
-        let liste = document.getElementById("colors");
-        let option = document.querySelector("value");
-        // ------ Création du produit qui sera ajouté au panier
-        let productAdded = {
-          image: imgProduit.src,
-          imageAlt: imgProduit.alt,
-          name: title.textContent,
+  console.log(itemIndex);
+//Modif de la quantité
+  const cartItemQuantity = +ItemPanier.Item[itemIndex].quantity
+  const quantityToAdd = +QuantitySelec
 
+  ItemPanier.Item[itemIndex].quantity = cartItemQuantity + quantityToAdd
 
+  localStorage.setItem("ItemPanier", JSON.stringify(ItemPanier))
 
-          
-          
-          price: parseFloat(prixProduit.textContent),
-          quantity: parseFloat(document.getElementById("quantity").value),
-          _id: id,
-        };
-
-        let arrayProduitPanier = [];
-      
-      // Si le localStorage existe, on récupère son contenu, on l'insère dans le tableau , puis on le renvoit vers le localStorage avec le nouveau produit ajouté.
-      if (localStorage.getItem("products") !== null) {
-        arrayProduitPanier = JSON.parse(localStorage.getItem("products"));
-        
-      }
-        // Si le LS est vide, on le crée avec le produit ajouté
-     
-        arrayProduitPanier.push(productAdded);
-        localStorage.setItem("products", JSON.stringify(arrayProduitPanier));
-      
-      console.log (arrayProduitPanier);
-        
-    } 
-  });
+  // Sinon push dans le tableau 
+} else {
+  console.log(ItemPanier);
+  ItemPanier.Item.push({"id" : id, "color" : Couleurselec, "quantity" : QuantitySelec})
+  localStorage.setItem("ItemPanier", JSON.stringify(ItemPanier))
 }
+
+//Vérification si localstorage contient déja des données
+if (typeof localStorage.ItemPanier !== "undefined"){
+  const pushItems = JSON.parse(localStorage.getItem("ItemPanier"))
+  let NbrItems = pushItems.Item.lenght -1
+
+  // Si il y a des données, push dans le tableau ItemPanier
+  for (let n=0; n<= NbrItems; n++){
+    ItemPanier.Item.push({
+      "id" : pushItems.Item[`${n}`].id,
+      "color" : pushItems.Item[`${n}`].color,
+      "quantity" : pushItems.Item[`${n}`].quantity
+    })
+  }
+}
+
+
+
+})}
